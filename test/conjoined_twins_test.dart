69716ceabb821cae9c40ef12082e48d904d6d910
Marek Gocał <marek@allbright.io>
2021-01-19 19:56:51 +0100

    add base files for test smells/antipatterns

---
+++ test/conjoined_twins_test.dart
@@ -0,0 +1,19 @@
+import 'package:test/test.dart';
+
+class Counter {
+  int value = 0;
+
+  void increment() => value++;
+
+  void decrement() => value--;
+}
+
+void main() {
+  test('Counter value should be incremented', () {
+    final counter = Counter();
+
+    counter.increment();
+
+    expect(counter.value, 1);
+  });
+}
